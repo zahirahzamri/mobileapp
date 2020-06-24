dcd17a0f693f4cf205d00bb2c90f6f8af35a8229
Nur Zahirah Zamri <zahirah.zamri@live.iium.edu.my>
2020-06-24 12:34:43 +0800

    Update Styles.js

--- wordAnalyzer/Styles.js
+++ wordAnalyzer/Styles.js
@@ -6,7 +6,7 @@ export const styles = StyleSheet.create({
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: '#F5FCFF',
-      marginTop:25,
+      marginTop:15,
     },
   title: {
       fontSize: 20,
